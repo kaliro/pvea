3d04e9a77a46849b9dce5519d0b51bedd4d3222b
Ami Cole <contact@amicole.co>
2020-07-10 16:32:32 -0500

    add example.js

---
+++ example.js
@@ -0,0 +1,40 @@
+// pvea example file
+const pveajs = require('./lib/pvea.js');
+
+// format: domain, username@authtype, password.
+// example: proxmox.example.org, admin@pve, hunter2.
+const pvea = new pveajs('xxx', 'xxx@pve', 'xxx')
+
+async function main() {
+
+  pvea.getVersion().then( res => {
+    console.log(res)
+  })
+
+  pvea.getNodes().then( res => {
+    console.log(res)
+  })
+
+  pvea.getNodeVersion('xxx').then( res => {
+    console.log(res)
+  })
+
+  pvea.getNodeTime('xxx').then( res => {
+    console.log(res)
+  })
+
+  pvea.getNodeLog('xxx', { limit: '3', since: '2020-07-07' }).then( res => {
+    console.log(res)
+  })
+
+  pvea.getNodeTasks('xxx', { limit: '3', vmid: '106' }).then( res => {
+    console.log(res)
+  })
+
+  pvea.getNodeStatus('xxx').then( res => {
+    console.log(res)
+  })
+
+}
+
+pvea.run(main)
